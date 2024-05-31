a26d42efb3a1ae1244ce491f4d44305e748e3a70
Juan Pablo Garcia <juagarciac@unal.edu.co>
2024-05-30 22:49:53 -0500

    primer intento

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,24 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
+def limpiar_monto(monto):
+    monto = monto.replace("$", "").replace(".", "").replace(",", "")
+    return monto
 
+def limpiar_fecha(entrada):
+    fecha = entrada.split("/")
+    if(len(fecha[0]) > 2):
+        return fecha[2] + "/" + fecha[1] + "/" + fecha[0]
+    else:
+        return entrada
+    
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = df.drop_duplicates().drop(df.columns[0], axis=1)
+    df = df.dropna()
+    df["monto_del_credito"] = df["monto_del_credito"].apply(limpiar_monto)
+    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(limpiar_fecha)
     return df
+
+#print(clean_data())
