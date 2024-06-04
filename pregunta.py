87f6aab40fd29b8a705ac31b0cac175710749c20
juagarciac <111471638+juagarciac@users.noreply.github.com>
2024-06-04 17:47:58 -0500

    3 intento

--- pregunta.py
+++ pregunta.py
@@ -8,24 +8,66 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-def limpiar_monto(monto):
-    monto = monto.replace("$", "").replace(".", "").replace(",", "")
-    return monto
 
-def limpiar_fecha(entrada):
-    fecha = entrada.split("/")
-    if(len(fecha[0]) > 2):
-        return fecha[2] + "/" + fecha[1] + "/" + fecha[0]
-    else:
-        return entrada
-    
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df = df.drop_duplicates().drop(df.columns[0], axis=1)
-    df = df.dropna()
-    df["monto_del_credito"] = df["monto_del_credito"].apply(limpiar_monto)
-    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(limpiar_fecha)
+    def limpiar_sexo(df):
+        df = df.copy()
+        df.sexo = df.sexo.str.lower()
+        return df
+
+    def limpiar_emprendimiento(df):
+        df = df.copy()
+        df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+        return df
+    
+    def limpiar_barrio(df):
+        df = df.copy()
+        df["barrio"] = df["barrio"].str.lower()
+        return df
+    
+    def limpiar_negocio(df):
+        df = df.copy()
+        df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip()
+        return df
+    
+    def limpiar_credito(df):
+        df = df.copy()
+        df["línea_credito"] = df["línea_credito"].str.lower().str.strip()
+        return df
+
+    def limpiar_comuna(df):
+        df = df.copy()
+        df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+        return df
+    
+    def limpiar_fecha(df):
+        df = df.copy()
+        df["fecha_de_beneficio"] = pd.to_datetime(
+        df.fecha_de_beneficio, format="%d/%m/%Y", errors="coerce"
+        ).fillna(pd.to_datetime(df.fecha_de_beneficio, format="%Y/%m/%d", errors="coerce"))
+        return df
+    
+    def limpiar_credito(df):
+        df = df.copy()
+        df.monto_del_credito = df.monto_del_credito.str.rstrip()
+        df.monto_del_credito = df.monto_del_credito.replace("[,$]", "", regex=True)
+        df.monto_del_credito = df.monto_del_credito.replace("(\\.00$)", "", regex=True)
+        df.monto_del_credito = df.monto_del_credito.astype(float)
+        return df
+    
+    
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df = limpiar_sexo(df)
+    df = limpiar_emprendimiento(df)
+    df = limpiar_barrio(df)
+    df = limpiar_negocio(df)
+    df = limpiar_credito(df)
+    df = limpiar_comuna(df)
+    df = limpiar_fecha(df)
+    df = limpiar_credito(df)
+    df = df.drop_duplicates().dropna()
     return df
 
 print(clean_data())
